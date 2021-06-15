d0b001cdae915115896f6b13b78a03f3a43b2e82
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-06-15 20:06:19 +0530

    add validation pipe

--- nestjs-demo/src/products/dto/product.dto.ts
+++ nestjs-demo/src/products/dto/product.dto.ts
@@ -1,6 +1,20 @@
+import { IsInt, IsNotEmpty, IsNumber, IsString } from "class-validator";
+
 export class createProductDTO{
+
+    @IsNotEmpty()
+    @IsString()
     readonly id: string;
+
+    @IsNotEmpty()
+    @IsString()
     readonly name: string;
+
+    @IsNotEmpty()
+    @IsInt()
     readonly qty: number;
+
+    @IsNotEmpty()
+    @IsNumber()
     readonly price: number;
 }
